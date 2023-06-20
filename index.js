7b142adb2c51dfa06f0e52833b33bebc91c88999
Luis andrade <andradex.js07@gmail.com>
2023-06-20 16:50:35 -0400

    feat: readJsonFile

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
+const util = require('util');
 
-const main = () => {
+const readFile = util.promisify(fs.readFile);
 
+const ReadJSONFile = async (path) => {
+  try {
+    const data = await readFile(path, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw { message: 'File Does Not Exist' };
+    } else if (err.code === 'EISDIR') {
+      throw { message: 'Path is a directory' };
+    } else if (err instanceof SyntaxError) {
+      throw { message: 'JSON Invalid' };
+    } else {
+      throw err;
+    }
+  }
 };
 
-
-
-module.exports = main;
+module.exports = ReadJSONFile;
